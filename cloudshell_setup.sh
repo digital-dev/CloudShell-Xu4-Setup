1bdcf813477456e9c22f3550f5eeff4eb93a6f2d
Pestilence <tvajgert@gmail.com>
2018-08-01 23:10:30 -0500

    CPU Governer Persistence

--- cloudshell_setup.sh
+++ cloudshell_setup.sh
@@ -1,16 +1,17 @@
 #!/bin/bash
 # Initial Setup
-apt update && apt upgrade && apt install curl sysstat vim git software-properties-common build-essential -y
+apt update && apt upgrade && apt install samba samba-common checkinstall rpm curl gawk bc sysstat vim git software-properties-common build-essential -y
 
 # Setup LCD Screen
-echo "spidev" >> /etc/modules
-echo "fbtft_device" >> /etc/modules
-echo "spi_s3c64xx" >> /etc/modules
+echo -e '\033[9;0]' > /dev/tty1
+echo -e 'spi_s3c64xx\nspidev\nfbtft_device' >> /etc/modules
 echo "options fbtft_device name=hktft9340 busnum=1 rotate=270" > /etc/modprobe.d/odroid-cloudshell.conf
-echo -e "# blacklist IO Board Sensors\nblacklist ioboard_bh1780\nblacklist ioboard_bmp180\nblacklist ioboard_keyled" > /etc/modprobe.d/blacklist-odroid.conf
-git clone https://github.com/mdrjr/cloudshell_lcd && ./cloudshell_lcd/build_deb.sh
-
+echo -e "# blacklist IO Board Sensors\nblacklist ioboard_bh1780\nblacklist ioboard_bmp180\nblacklist ioboard_keyled\n# LCD Touchscreen driver\nblacklist ads7846" > /etc/modprobe.d/blacklist-odroid.conf
+sed -i -e 's/console=tty1/console=tty1 consoleblank=0/g' /media/boot/boot.ini
 # Setup CPU Throttling
 wget https://raw.githubusercontent.com/mad-ady/odroid-cpu-control/master/odroid-cpu-control && chmod +x odroid-cpu-control
-sudo chmod a-x /etc/init.d/ondemand # Disables default CPU Governor
+#sudo chmod a-x /etc/init.d/ondemand # Disables default CPU Governor
 ./odroid-cpu-control -s -g "powersave" -m 200m -M 1.4G
+# Persistent CPU Throttling
+sed -i '13i\\n./usr/bin/odroid-cpu-control -s -g powersave -m 200 -M 1.4G' /etc/rc.local
+mv odroid-cpu-control /usr/bin/odroid-cpu-control
